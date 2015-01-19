0a14573c354e85ce31823427079e8e2b7eed9779
Doug <clarke.doug248@gmail.com>
2015-01-19 13:51:02 -0500

    refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   end
 
   def create
-     @post = Post.new(params.require(:post).permit(:title, :body))
+     @post = Post.new(post_params)
      authorize @post     
      if @post.save
        flash[:notice] = "Post was saved."
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
   def update
      @post = Post.find(params[:id])
      authorize post
-     if @post.update_attributes(params.require(:post).permit(:title, :body))
+     if @post.update_attributes(post_params)
        flash[:notice] = "Post was updated."
        redirect_to @post
      else
@@ -41,5 +41,11 @@ class PostsController < ApplicationController
        render :edit
      end
   end
-   
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end 
+
 end

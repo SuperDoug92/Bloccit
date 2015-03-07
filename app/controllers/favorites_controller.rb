0e5b22eda085e42d2d43e62b7021c43380c5826c
Doug <clarke.doug248@gmail.com>
2015-03-07 17:49:56 -0500

    favorites and action mailer, mailer errors

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,27 @@
 class FavoritesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.build(post: @post)
+    authorize favorite
+    if favorite.save
+      flash[:notice] = "Favorite saved."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "There was an error. Please try again."
+      render 'posts/show'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find(params[:id])
+    authorize favorite
+    if favorite.destroy
+      flash[:notice] = "Unfavorited."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "There was an error. Please try again."
+      render 'posts/show'
+    end
+  end
 end
